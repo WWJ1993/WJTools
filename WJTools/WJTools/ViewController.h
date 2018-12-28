24a9fe412d30ad9fa9fd7f09f62e091f26de2f4c
WWJ <1042086383@qq.com>
2018-12-28 17:37:04 +0800

    1

---
+++ WJTools/WJTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJTools
+//
+//  Created by WWJ on 2018/12/28.
+//  Copyright © 2018年 WWJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WJTools/WJTools/WJTools/WJTools.h
@@ -0,0 +1,13 @@
+//
+//  WJTools.h
+//  WJTools
+//
+//  Created by WWJ on 2018/12/28.
+//  Copyright © 2018年 WWJ. All rights reserved.
+//
+
+#ifndef WJTools_h
+#define WJTools_h
+
+
+#endif /* WJTools_h */
